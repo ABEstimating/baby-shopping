ed02f672b01517316c1bf49c06995c03b65bbd51
ABEstimating <bandrews@autobuilders.net>
2026-09-11 22:25:10 -0400

    Refresh WLA hero and fixed cover cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baby-shopping-v7';
+const CACHE='baby-shopping-v8';
 const ASSETS=['./','index.html','modern.css','modern.js','manifest.json','icon.svg','wla-family-hero.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
