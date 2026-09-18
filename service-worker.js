0946be572aada9f0fe1eacd0147fa91e3e0666ac
ABEstimating <bandrews@autobuilders.net>
2026-09-18 17:07:37 -0400

    Refresh checklist offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='personal-checklist-v3';
+const CACHE='personal-checklist-v4';
 const ASSETS=['./','index.html','modern.css','modern.js','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
